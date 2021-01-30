22b4850dc4d70482cfaca22f0a76d3e70e4a33b5
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2021-01-30 12:31:38 +0200

    Revert "Підєднав паке tui-pagination"
    This reverts commit 226f895c5392776708542e33b3c04142781b8dc2.

--- src/JS/paginationForMovie.js
+++ src/JS/paginationForMovie.js
@@ -1,4 +1,3 @@
-// ============ Bootstrap ===================
 const current = document.querySelector(".current")
 current.classList.add("active")
 
@@ -25,40 +24,3 @@ buttonNext.addEventListener("click", event => {
   array.forEach(item => console.log(item.dataset.index))
 }
 )
-
-// ============ Tui-pagination ===================
-
-import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
-require('tui-pagination')
-
-const container = document.getElementById('tui-pagination-container');
-const options = { // below default value of options
-     totalItems: 500,
-     itemsPerPage: 9,
-     visiblePages: 8,
-     page: 1,
-     centerAlign: false,
-     firstItemClassName: 'tui-first-child',
-     lastItemClassName: 'tui-last-child',
-     template: {
-         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-         moveButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</a>',
-         disabledMoveButton:
-             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</span>',
-         moreButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
-             '</a>'
-     }
-};
-const pagination = new Pagination(container, options);
-pagination.getCurrentPage();
-
-
